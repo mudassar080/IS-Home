4bac1f75e0a00e8185ead3b5df97a1e46b15354d
mudassar-ubiquify <151502348+mudassar-ubiquify@users.noreply.github.com>
2024-10-02 12:33:39 +0500

    price page faq section updated

--- src/pages/pricing/index.js
+++ src/pages/pricing/index.js
@@ -95,7 +95,11 @@ const Page = () => {
                     priceRange="650"
                     features={[
                       "Provisional Patent Application",
-                      "After 1 Year You have to File Another Provisional or a Non Provisional",
+                      <span>
+                        After <b className="text-[#000] font-bold">1</b> Year
+                        You have to File Another Provisional or a Non
+                        Provisional
+                      </span>,
                       "Doesn't Allow You to Enforce Your Patent.",
                     ]}
                     selected=""
@@ -190,125 +194,125 @@ const Page = () => {
                   Frequently Asked Questions
                 </h3>
               </div>
-              <div className="row max-[992px]:flex-col">
-                <div className="w-4/12 ml-auto max-[992px]:mr-auto max-[992px]:w-full max-w-[360px] pt-[70px] pb-[30px]">
-                  <div className="flex items-center gap-2.5">
-                    <Image
-                      src="/membershipCard.svg"
-                      width={36}
-                      height={36}
-                      className="mb-auto w-[36px] h-[36px]"
-                    />
-                    <div>
-                      <p className="text-lg font-bold">
-                        Will I have a real Lawyer?
-                      </p>
-                      <TextBlock className="text-sm">
-                        Yes, we will partner you with an official US lawyer.
-                      </TextBlock>
+              <div className="flex flex-wrap max-[992px]:flex-col pt-[70px]">
+                <div className="w-1/2 pr-[10px] max-[992px]:ml-auto max-[992px]:mr-auto max-[992px]:pr-[0px]">
+                  <div className="ml-auto max-[992px]:mr-auto max-[992px]:w-full max-w-[360px] pb-[30px]">
+                    <div className="flex items-center gap-2.5">
+                      <Image
+                        src="/membershipCard.svg"
+                        width={36}
+                        height={36}
+                        className="mb-auto w-[36px] h-[36px]"
+                      />
+                      <div>
+                        <p className="text-lg font-bold">
+                          Will I have a real Lawyer?
+                        </p>
+                        <TextBlock className="text-sm">
+                          Yes, we will partner you with an official US lawyer.
+                        </TextBlock>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="ml-auto max-[992px]:mr-auto max-[992px]:w-full max-w-[360px] pb-[30px]">
+                    <div className="flex items-center gap-2.5">
+                      <Image
+                        src="/money.svg"
+                        width={36}
+                        height={36}
+                        className="mb-auto w-[36px] h-[36px] -mt-1"
+                      />
+                      <div>
+                        <p className="text-lg font-bold">
+                          Can I cancel or back out?
+                        </p>
+                        <TextBlock className="text-sm">
+                          Yes at any time. <br /> The lawyer will ask for half
+                          of the payment upfront and you will only be charged
+                          for services that have been rendered. If part way
+                          through the process you want to back out you will be
+                          refunded proportional to the services alreadly
+                          rendered. Note you can pause your filing indefinetly
+                          with us if you need to wait for any reason.
+                        </TextBlock>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="ml-auto max-[992px]:mr-auto max-[992px]:w-full max-w-[360px] pb-[30px]">
+                    <div className="flex items-center gap-2.5">
+                      <Image
+                        src="/clock.png"
+                        width={36}
+                        height={36}
+                        className="mb-auto w-[36px] h-[36px] -mt-1"
+                      />
+                      <div>
+                        <p className="text-lg font-bold">
+                          What are the timelines like for the process?
+                        </p>
+                        <TextBlock className="text-sm">
+                          Sign up and filling out the filing forms takes ~5
+                          minutes From the moment of payment you can see your
+                          first draft within 48 hours
+                        </TextBlock>
+                      </div>
                     </div>
                   </div>
                 </div>
-                <div className="w-4/12 mr-auto max-[992px]:ml-auto max-[992px]:w-full max-w-[360px] pt-[70px] pb-[30px]">
-                  <div className="flex items-center gap-2.5">
-                    <Image
-                      src="/giftCard.svg"
-                      width={36}
-                      height={36}
-                      className="mb-auto w-[36px] h-[36px] -mt-1"
-                    />
-                    <div>
-                      <p className="text-lg font-bold">
-                        What does a Patent do?
-                      </p>
-                      <TextBlock className="text-sm">
-                        Patents protect your innovative ideas and inventions,
-                        granting you exclusive rights for 20 years from the
-                        filing date. If a competitor tries to enter the market
-                        with an infringing product or service, you can take
-                        legal action to protect your interests. This may involve
-                        issuing a cease and desist order or negotiating a
-                        licensing agreement for royalty payments. By securing a
-                        patent, you safeguard your competitive edge and prevent
-                        unauthorized use of your intellectual property.
-                      </TextBlock>
+                <div className="w-1/2 pl-[10px] max-[992px]:ml-auto max-[992px]:mr-auto max-[992px]:pl-[0px]">
+                  <div className="mr-auto max-[992px]:ml-auto max-[992px]:w-full max-w-[360px] pb-[30px]">
+                    <div className="flex items-center gap-2.5">
+                      <Image
+                        src="/giftCard.svg"
+                        width={36}
+                        height={36}
+                        className="mb-auto w-[36px] h-[36px] -mt-1"
+                      />
+                      <div>
+                        <p className="text-lg font-bold">
+                          What does a Patent do?
+                        </p>
+                        <TextBlock className="text-sm">
+                          Patents protect your innovative ideas and inventions,
+                          granting you exclusive rights for 20 years from the
+                          filing date. If a competitor tries to enter the market
+                          with an infringing product or service, you can take
+                          legal action to protect your interests. This may
+                          involve issuing a cease and desist order or
+                          negotiating a licensing agreement for royalty
+                          payments. By securing a patent, you safeguard your
+                          competitive edge and prevent unauthorized use of your
+                          intellectual property.
+                        </TextBlock>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="mr-auto max-[992px]:ml-auto max-w-[360px] max-[992px]:w-full pb-[30px]">
+                    <div className="flex items-center gap-2.5">
+                      <Image
+                        src="/message.svg"
+                        width={36}
+                        height={36}
+                        className="mb-auto w-[36px] h-[36px]"
+                      />
+                      <div>
+                        <p className="text-lg font-bold">
+                          Why should I use Introstellar?
+                        </p>
+                        <TextBlock className="text-sm	mb-[10px]">
+                          We advocate and empower you the client. In the
+                          standard patent process you are at the whims of
+                          whatever lawyer you end up with. At Introstellar we
+                          provide you with, a perfect and streamlined experience
+                          to make the application process as simple as ordering
+                          an Uber. We also provide FREE, well produced
+                          educational content to help you understand the process
+                          and also customer support for any issues.
+                        </TextBlock>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-              <div className="row max-[992px]:flex-col">
-                <div className="w-4/12 ml-auto max-[992px]:mr-auto max-[992px]:w-full max-w-[360px] pt-[70px] pb-[30px]">
-                  <div className="flex items-center gap-2.5">
-                    <Image
-                      src="/money.svg"
-                      width={36}
-                      height={36}
-                      className="mb-auto w-[36px] h-[36px] -mt-1"
-                    />
-                    <div>
-                      <p className="text-lg font-bold">
-                        Can I cancel or back out?
-                      </p>
-                      <TextBlock className="text-sm">
-                        Yes at any time. <br /> The lawyer will ask for half of
-                        the payment upfront and you will only be charged for
-                        services that have been rendered. If part way through
-                        the process you want to back out you will be refunded
-                        proportional to the services alreadly rendered. Note you
-                        can pause your filing indefinetly with us if you need to
-                        wait for any reason.
-                      </TextBlock>
-                    </div>
-                  </div>
-                </div>
-                <div className="w-4/12 mr-auto max-[992px]:ml-auto max-w-[360px] max-[992px]:w-full pt-[70px] pb-[30px]">
-                  <div className="flex items-center gap-2.5">
-                    <Image
-                      src="/message.svg"
-                      width={36}
-                      height={36}
-                      className="mb-auto w-[36px] h-[36px]"
-                    />
-                    <div>
-                      <p className="text-lg font-bold">
-                        Why should I use Introstellar?
-                      </p>
-                      <TextBlock className="text-sm	mb-[10px]">
-                        We advocate and empower you the client. In the standard
-                        patent process you are at the whims of whatever lawyer
-                        you end up with. At Introstellar we provide you with, a
-                        perfect and streamlined experience to make the
-                        application process as simple as ordering an Uber. We
-                        also provide FREE, well produced educational content to
-                        help you understand the process and also customer
-                        support for any issues.
-                      </TextBlock>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="row max-[992px]:flex-col">
-                <div className="w-4/12 ml-auto max-[992px]:mr-auto max-[992px]:w-full max-w-[360px] pt-[70px] pb-[30px]">
-                  <div className="flex items-center gap-2.5">
-                    <Image
-                      src="/clock.png"
-                      width={36}
-                      height={36}
-                      className="mb-auto w-[36px] h-[36px] -mt-1"
-                    />
-                    <div>
-                      <p className="text-lg font-bold">
-                        What are the timelines like for the process?
-                      </p>
-                      <TextBlock className="text-sm">
-                        Sign up and filling out the filing forms takes ~5
-                        minutes From the moment of payment you can see your
-                        first draft within 48 hours
-                      </TextBlock>
-                    </div>
-                  </div>
-                </div>
-                <div className="w-4/12 mr-auto max-[992px]:ml-auto max-w-[360px] max-[992px]:w-full pt-[70px] pb-[30px]"></div>
               </div>
             </div>
             <div className="w-max mx-auto max-[770px]:w-full mt-10">
